4f57a26a8120d53ccdc0b9575f97b56a9a66fae8
carlo <carlo@DESKTOP-NAD22D3.home>
2021-01-11 12:50:51 +0100

    Implement class DAO

--- src/main/DAO.java
+++ src/main/DAO.java
@@ -1,5 +1,34 @@
 package main;
 
-public class DAO {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
+public class DAO {
+	
+	private Connection conn;
+	
+	public  DAO() {
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			try {
+				conn = DriverManager.getConnection("jdbc:mysql://den1.mysql2.gear.host/spotyfree", "spotyfree", "ivan@2020");
+			} catch (SQLException e) {
+				JOptionPane.showMessageDialog(null, "Connessione al server fallita", "ERRORE", JOptionPane.ERROR_MESSAGE);
+			}
+		} catch (ClassNotFoundException e1) {
+			e1.printStackTrace();
+		}
+	}
+
+	public Connection getConn() {
+		return conn;
+	}
+
+	public void setConn(Connection conn) {
+		this.conn = conn;
+	}
 }
