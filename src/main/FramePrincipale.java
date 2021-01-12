8571449758d1a8f67d35bd1d980ac8176ec9484a
carlo <carlo@DESKTOP-NAD22D3.home>
2021-01-12 13:43:57 +0100

    Add images and update framePrincipale

--- src/main/FramePrincipale.java
+++ src/main/FramePrincipale.java
@@ -6,15 +6,16 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import java.awt.Window.Type;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Cursor;
 
 public class FramePrincipale extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -28,9 +29,6 @@ public class FramePrincipale extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
 	public FramePrincipale() {
 		setTitle("CtrlFood");
 		setResizable(false);
@@ -38,8 +36,49 @@ public class FramePrincipale extends JFrame {
 		setBounds(100, 100, 1152, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton ButtonDeposito = new JButton("");
+		ButtonDeposito.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		ButtonDeposito.setContentAreaFilled(false);
+		ButtonDeposito.setBorderPainted(false);
+		ButtonDeposito.setVisible(false);
+		ButtonDeposito.setOpaque(false);
+		ButtonDeposito.setIcon(new ImageIcon(FramePrincipale.class.getResource("/scrimg/Button_Deposito.png")));
+		ButtonDeposito.setBounds(59, 97, 223, 52);
+		contentPane.add(ButtonDeposito);
+		
+		JButton ButtonNegozio = new JButton("");
+		ButtonNegozio.setIcon(new ImageIcon(FramePrincipale.class.getResource("/scrimg/Button_Negozio.png")));
+		ButtonNegozio.setVisible(false);
+		ButtonNegozio.setOpaque(false);
+		ButtonNegozio.setContentAreaFilled(false);
+		ButtonNegozio.setBorderPainted(false);
+		ButtonNegozio.setBounds(59, 200, 223, 52);
+		contentPane.add(ButtonNegozio);
+		
+		JButton ButtonCliente = new JButton("");
+		ButtonCliente.setIcon(new ImageIcon(FramePrincipale.class.getResource("/scrimg/Button_Cliente.png")));
+		ButtonCliente.setVisible(false);
+		ButtonCliente.setOpaque(false);
+		ButtonCliente.setContentAreaFilled(false);
+		ButtonCliente.setBorderPainted(false);
+		ButtonCliente.setBounds(59, 300, 223, 52);
+		contentPane.add(ButtonCliente);
+		
+		JButton ButtonCarello = new JButton("");
+		ButtonCarello.setIcon(new ImageIcon(FramePrincipale.class.getResource("/scrimg/Button_Carrello.png")));
+		ButtonCarello.setVisible(false);
+		ButtonCarello.setOpaque(false);
+		ButtonCarello.setContentAreaFilled(false);
+		ButtonCarello.setBorderPainted(false);
+		ButtonCarello.setBounds(59, 400, 223, 52);
+		contentPane.add(ButtonCarello);
+		
+		JLabel Homepage = new JLabel("");
+		Homepage.setIcon(new ImageIcon(FramePrincipale.class.getResource("/scrimg/Homepage.png")));
+		Homepage.setBounds(0, 0, 1146, 571);
+		contentPane.add(Homepage);
 	}
-
 }
